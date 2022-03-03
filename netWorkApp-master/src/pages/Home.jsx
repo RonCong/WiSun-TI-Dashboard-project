85e56e63abae636ab72dfb3ef439583292e84c22
bloxx8 <72635672+bloxx8@users.noreply.github.com>
2022-03-02 18:20:28 -0600

    Ron: Remade sections, implemented graph

--- netWorkApp-master/src/pages/Home.jsx
+++ netWorkApp-master/src/pages/Home.jsx
@@ -1,149 +1,22 @@
 import { useState, useEffect } from "react";
-import SliderInput from "../components/SliderInput";
-import ErrorRate from '../components/ErrorRate'
-import Delay from '../components/Delay'
-import LineChart from '../components/LineChart'
+import MyLineChart from '../components/MyLineChart'
 import "./Home.css";
 import {
   Layout,
   PageHeader,
-  Button,
-  Descriptions,
   Row,
   Col,
-  Switch,
-  Table,
-  Tag,
-  Tooltip,
-  Progress
+  Select
 } from "antd";
 
-const { Header, Content, Footer } = Layout;
-
+const { Content, Footer } = Layout;
+const { Option } = Select;
 function Home() {
-  const [grade, setGrade] = useState('A+');
-  const thirdData = [
-    {
-      key: '1',
-      ip: 'abcd:abcd:abcd:abcd',
-      nickName: '',
-      status: 'success',
-    },
-    {
-      key: '2',
-      ip: 'abcd',
-      nickName: '',
-      status: 'success',
-    },
-    {
-      key: '3',
-      ip: 'abcd',
-      nickName: 'Alpha',
-      status: 'success',
-    },
-    {
-      key: '4',
-      ip: 'abcd',
-      nickName: 'Alpha',
-      status: 'error',
-    },
-  ];
+  const [timeSlot, setTimeSlot] = useState(2);
 
-  const thirdColumns = [
-    {
-      title: 'IP',
-      dataIndex: 'ip',
-      key: 'ip',
-      render: value => (
-        <Tooltip title={value}>
-          <span>{value}</span>
-        </Tooltip>
-      )
-    },
-    {
-      title: 'NickName',
-      dataIndex: 'nickName',
-      key: 'nickName',
-    },
-    {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
-      render: value => (
-        <Tag color={value}> {value} </Tag>
-      ),
-    },
-  ];
-
-  const fourthData = [
-    {
-      key: '1',
-      id: 'adba',
-      start: '10/10 10:49',
-      duration: 120,
-      errorRate: 20
-    },
-    {
-      key: '2',
-      id: 'adba',
-      start: '10/10 10:49',
-      duration: 220,
-      errorRate: 50
-    },
-    {
-      key: '3',
-      id: 'adba',
-      start: '10/10 10:49',
-      duration: 50,
-      errorRate: 40
-    },
-    {
-      key: '4',
-      id: 'adba',
-      start: '10/10 10:49',
-      duration: 10,
-      errorRate: 62
-    },
-  ];
-
-  const fourthColumns = [
-    {
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
-    },
-    {
-      title: 'Start',
-      dataIndex: 'start',
-      key: 'start',
-    },
-    {
-      title: 'Duration(ms)',
-      dataIndex: 'duration',
-      key: 'duration',
-      render: value => (
-        <Tooltip title={`${value}ms`}>
-          <Progress
-            percent={value / 250 * 100}
-            strokeColor={value > 200 ? '#FB4934' : (value > 100 ? '#FABD2F' : '#8EC07C')}
-            showInfo={false} />
-        </Tooltip>
-      )
-    },
-    {
-      title: 'Error Rate',
-      dataIndex: 'errorRate',
-      key: 'errorRate',
-      render: value => (
-        <Tooltip title={`${value}ms`}>
-          <Progress
-            percent={value}
-            strokeColor={value > 60 ? '#FB4934' : (value > 30 ? '#FABD2F' : '#8EC07C')}
-            showInfo={false} />
-        </Tooltip>
-      )
-    },
-  ];
+  const handleChange = (e) => {
+    setTimeSlot(e);
+  }
 
   useEffect(() => { });
 
@@ -153,74 +26,21 @@ function Home() {
         <PageHeader
           className="site-page-header"
           title="Your NetWork"
-          extra={[<Switch key="1" />]}
+          extra={
+            <>
+              <span style={{ fontSize: '16px', fontWeight: '700' }}> Time: </span><Select defaultValue={timeSlot} style={{ width: 120 }} onChange={handleChange}>
+                <Option value={0}>00:00-08:00</Option>
+                <Option value={1}>00:00-16:00</Option>
+                <Option value={2}>00:00-24:00</Option>
+              </Select>
+            </>}
         />
-        <Content style={{ padding: "0 50px" }}>
-          <main className="site-layout-content">
-            <Row gutter={[30, 24]}>
-              <Col span={12}>
-                <div className="first">
-                  <Descriptions
-                    title="Ping Config"
-                    bordered
-                    extra={<Button>submit</Button>}
-                  >
-                    <Descriptions.Item
-                      label="Packet Size"
-                      span={3}
-                    >
-                      <SliderInput></SliderInput>
-                    </Descriptions.Item>
-                    <Descriptions.Item
-                      label="Timeout"
-                      span={3}
-                    >
-                      <SliderInput></SliderInput>
-                    </Descriptions.Item>
-                    <Descriptions.Item
-                      label="Interval"
-                      span={3}
-                    >
-                      <SliderInput></SliderInput>
-                    </Descriptions.Item>
-                    <Descriptions.Item
-                      label="Number of Packets"
-                      span={3}
-                    >
-                      <SliderInput></SliderInput>
-                    </Descriptions.Item>
-                  </Descriptions>
-                </div>
-              </Col>
-              <Col span={12}>
-                <div className="second">
-                  <Descriptions
-                    title="At A Glance"
-                    bordered
-                    extra={<div style={{ fontSize: '23px' }}>Grade: {grade}</div>}
-                  >
-                    <div>
-                      <ErrorRate></ErrorRate>
-                    </div>
-                    <div>
-                      <Delay></Delay>
-                    </div>
-                  </Descriptions>
-                </div>
-              </Col>
-              <Col span={12}>
-                <div className="third">
-                  <Table rowSelection title={() => 'IP Addresses'} pagination={false} columns={thirdColumns} dataSource={thirdData} />
-                </div>
-              </Col>
-              <Col span={12}>
-                <div className="fourth">
-                  <Table rowSelection title={() => 'IP Addresses'} pagination={false} columns={fourthColumns} dataSource={fourthData} />
-                </div>
-              </Col>
+        <Content style={{ padding: "0 50px" }} >
+          <main className="site-layout-content" >
+            <Row>
               <Col span={24}>
-                <div className="fifth">
-                  <LineChart></LineChart>
+                <div className="first">
+                  <MyLineChart timeSlot={timeSlot}></MyLineChart>
                 </div>
               </Col>
             </Row>
